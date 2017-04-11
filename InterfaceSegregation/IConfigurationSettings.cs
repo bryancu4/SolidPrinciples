3a308705ae1b9f680a0c5a5f779220175d997597
Bryan Cua <bryancu4@gmail.com>
2017-04-11 23:32:55 +0100

    Problem

---
+++ InterfaceSegregation/IConfigurationSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace InterfaceSegregation
+{
+    public interface IConfigurationSettings
+    {
+        // application identity settings
+        string ApplicationName { get; }
+        string AuthorName { get; set; }
+
+        // performance tuning settings
+        int CacheDuration { get; }
+
+        // data access settings
+        string DatabaseServerName { get; }
+        string DatabaseName { get; }
+        string DatabaseUserName { get; }
+        string DatabasePassword { get; }
+        
+        // web service api settings
+        string WebServiceBaseUri { get; }
+    }
+}
